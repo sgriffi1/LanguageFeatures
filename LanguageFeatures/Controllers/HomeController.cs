80342e64439b5f27c28ff974adef7e499733801c
steven griffiths <47143897+sgriffi1@users.noreply.github.com>
2020-06-15 16:58:08 +0100

    Add project files.

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                //results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
+            }
+
+            return View(results);
+            //return View(new string[] { "C#", "Language", "Features" });
+        }
+
+        public ViewResult IndexA()
+        {
+            string[] names = new string[3];
+            names[0] = "Bob";
+            names[1] = "Joe";
+            names[2] = "Alice";
+            return View("Index", names);
+        }
+        //ABOVE INDEX CAN BE WRITTEN AS BELOW
+
+        public ViewResult IndexB()
+        {
+            return View("Index", new string[] { "Bob", "Joe", "Alice" });
+        }
+
+    }
+}
